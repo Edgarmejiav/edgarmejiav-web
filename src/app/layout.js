07e9c8cf48b359e93636ea88028b3cfe8c667e82
edgarmejiav <edgarm_10@hotmail.com>
2023-07-16 13:14:59 -0500

    chors: metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,19 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import {NavBar} from "@/components/NavBar";
 import {Footer} from "@/components/footer/Footer";
+import {Metadata} from "next";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'EdgarMejiav - Home',
   description: ' Software engineer & web Developer',
+  image: 'https://avatars.githubusercontent.com/u/35704346?v=4',
+  twitter: {
+    title: 'EdgarMejiav - Home',
+    description: ' Software engineer & web Developer',
+    image: 'https://avatars.githubusercontent.com/u/35704346?v=4',
+  }
 }
 
 export default function RootLayout({ children }) {

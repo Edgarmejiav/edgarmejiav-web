fc7fe4d44e51b883d74c2d2b7568a82cd7e12efe
edgarmejiav <edgarm_10@hotmail.com>
2023-07-16 01:04:32 -0500

    fix: key card

--- src/components/apps/ConteinerApps.jsx
+++ src/components/apps/ConteinerApps.jsx
@@ -9,7 +9,7 @@ export const ConteinerApps = () => {
             <section
                 className=" container mx-auto lg:px-[100px] md:px-[50px]  px-[25px] mt-10 grid xl:grid-cols-2 grid-cols-1  gap-10 ">
                 {data.map((item, index) => {
-                    return <Card key={index} {...item} />
+                    return <Card key={item.title} {...item} />
                 })
                 }
             </section>

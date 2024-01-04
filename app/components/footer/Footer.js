51682baf37ac9dff557f235f7c0542a57c74e618
edgarmejiav <legamejiav@gmail.com>
2024-01-04 17:32:30 -0500

    style: hidden remove

--- app/components/footer/Footer.js
+++ app/components/footer/Footer.js
@@ -2,14 +2,14 @@ export const Footer = () => {
 
     return (
         <footer
-            className="border-t border-gray-200 dark:border-slate-800 mt-20 py-10 text-center">
+            className="border-t border-gray-200 dark:border-slate-800 mt-20 py-10 text-center justify-center flex flex-col md:flex-row">
 
-            <span className="hidden md:inline text-sm text-gray-700 mr-4 dark:text-slate-400">
+            <span className=" md:inline text-sm text-gray-700 mr-4 dark:text-slate-400">
               <q>
                     Cambiar el mundo a través del código
               </q>
             </span>
-            <span className="hidden md:inline text-sm text-gray-700 mr-4 dark:text-slate-400">
+            <span className=" md:inline text-sm text-gray-700 mr-4 dark:text-slate-400">
                 Hecho por EdgarMejiaV con ❤️
             </span>
         </footer>

15fdf64b2dc1d9cb772dc30e371034b6bd5d5090
edgar <legamejiav@gmail.com>
2024-03-01 17:43:04 -0500

    feat: remove min-h-screen and add hover to buttons

--- app/components/page/ContainerPage.js
+++ app/components/page/ContainerPage.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const ContainerPage = () => {
     return (
-        <div className="flex flex-col items-center justify-center sm:min-h-screen  min-h-[60vh]  gap-7  ">
+        <div className="flex flex-col items-center justify-center   min-h-[80vh]  gap-7  ">
             <h1 className="
              bg-gradient-to-br from-blue-900 to-lime-400
              text-transparent
@@ -16,14 +16,16 @@ export const ContainerPage = () => {
             <h1 className="sm:text-3xl text-1xl   font-bold text-slate-300">Software engineer & web Developer </h1>
 
             <div className="text-white flex flex-row gap-10">
-                <button className=" flex items-center gap-2 border border-neutral-800 p-3 rounded-2xl font-bold   ">
+                <button
+                    className=" flex items-center gap-2 border border-neutral-800 p-3 rounded-2xl font-bold hover:bg-neutral-800 hover:text-white  ">
                     <Link
                         className="flex items-center sm:p-2 p-1 sm:text-[20px] text-xs scroll-smooth hover:scroll-auto"
                         href="#apps">
                         Aplicaciones
                     </Link>
                 </button>
-                <button className=" flex items-center gap-2 border border-neutral-800 p-3 rounded-2xl font-bold   ">
+                <button className=" flex items-center gap-2 border border-neutral-800 p-3 rounded-2xl font-bold hover:bg-neutral-800 hover:text-white
+                   ">
                     <Link className="flex items-center sm:p-2 p-1 sm:text-[20px] text-xs " target="_blank"
                           href="https://github.com/Edgarmejiav">
                         Github

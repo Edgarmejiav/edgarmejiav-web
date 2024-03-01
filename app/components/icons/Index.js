1b34e130df6b79933fcbeca351ee80543bf2c45a
edgar <legamejiav@gmail.com>
2024-03-01 17:43:28 -0500

    delete: delete index icons

--- app/components/icons/Index.js
+++
@@ -1,4 +0,0 @@
-export {IconApps} from "@/app/components/icons/IconApps";
-export {IconGithub} from "@/app/components/icons/IconGithub";
-// export {IconInstagram} from "@/components/icons/IconInstagram";
-

8649467f6f4bb90b5935ed284ed60b8fdf53e1b8
edgarmejiav <legamejiav@gmail.com>
2024-06-10 13:08:17 -0500

    feat: add badge navbar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -9,10 +9,15 @@ export const NavBar = () => {
             className={' fixed top-0 py-10 backdrop-blur w-full flex  items-center justify-around h-14 text-white text-2xl font-semibold'}
         >
 
-            <div>
+            <div className={"flex gap-4 items-center"}>
                 <Link href="/">
                     <h1 className="text-xl sm:text-2xl">EdgarMejiaV</h1>
                 </Link>
+                <span
+                    className="bg-green-100 h-fit text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+                    Disponible para trabajar
+                </span>
+
             </div>
             <div className="flex flex-row gap-10">
 

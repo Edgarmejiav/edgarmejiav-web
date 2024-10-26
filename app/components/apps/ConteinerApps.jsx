85e0f24449ac711bc5dec6ab3954b4053e42e391
edgar <legamejiav@gmail.com>
2024-10-26 17:44:33 -0500

    ✨: rename

--- app/components/apps/ConteinerApps.jsx
+++ app/components/apps/ConteinerApps.jsx
@@ -17,7 +17,7 @@ export const ConteinerApps = () => {
             </section>
             <header className="  m-20 flex justify-center  ">
                 <h1 className=" bg-gradient-to-br from-blue-900 to-lime-400 text-transparent bg-clip-text
-            p-2 md:text-5xl sm:text-3xl  text-2xl font-bold">Tutoriales</h1>
+            p-2 md:text-5xl sm:text-3xl  text-2xl font-bold">Demos</h1>
             </header>
             <section
                 className=" container mx-auto lg:px-[100px] md:px-[50px]  px-[25px] mt-10 grid xl:grid-cols-2 grid-cols-1  gap-10 ">
